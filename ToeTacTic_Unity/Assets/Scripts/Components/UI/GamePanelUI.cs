ba85d14e736ebbc4b76a050780e9b37888358cb7
ryanyearsley <ryan.a.yearsley@gmail.com>
2023-10-23 02:03:25 -0400

    Add GamePanelUI (Extends AbstractPanelUI)

---
+++ ToeTacTic_Unity/Assets/Scripts/Components/UI/GamePanelUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePanelUI : AbstractPanelUI
+{
+	private TicTacToeTileButton[] tileButtons;
+
+	protected override void InitializePanel()
+	{
+		Debug.Log("GamePanelUI: Getting TileButtons in children");
+		tileButtons = GetComponentsInChildren<TicTacToeTileButton>();
+		base.InitializePanel();
+	}
+	protected override void OnUIChange(GameState gameState)
+	{
+		base.OnUIChange(gameState);
+		if (gameState == GameState.GAME) {
+			Debug.Log("GamePanelUI: Activating Game Panel, clearing tile buttons");
+			foreach (TicTacToeTileButton tile in tileButtons)
+			{
+				tile.ClearTile();
+			}
+		}
+	}
+}
