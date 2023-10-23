5c40e5ade5f9b79d36dfde6b1b1be97a0978fdb9
ryanyearsley <ryan.a.yearsley@gmail.com>
2023-10-23 02:02:52 -0400

    Add TicTacToeTileButton

---
+++ ToeTacTic_Unity/Assets/Scripts/Components/UI/TicTacToeTileButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TicTacToeTileButton : AbstractButtonClick
+{
+
+	[SerializeField]
+	private Vector2Int tileCoordinate;
+
+	private Image tileImage;
+
+	[SerializeField]
+	private Sprite emptySprite;
+	[SerializeField]
+	private Sprite xSprite;
+	[SerializeField]
+	private Sprite ySprite;
+
+	private bool isVacant = false;
+	protected override void Awake()
+	{
+		base.Awake();
+		tileImage = GetComponent<Image>();
+	}
+	public void ClearTile()
+	{
+		tileImage.sprite = emptySprite;
+		isVacant = true;
+	}
+	public override void OnClick()
+	{
+		if (isVacant == true && TicTacToeGameManager.instance != null  && TicTacToeGameManager.instance.GetGameState() == GameState.GAME)
+		{
+			Player player = TicTacToeGameManager.instance.OnGameTileClick(tileCoordinate);
+			tileImage.sprite = player == Player.X ? xSprite : ySprite;
+			isVacant = false;
+		}
+	}
+}
