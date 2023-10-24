7017363d24cbe360f044f0e3456f24540daf55f9
ryanyearsley <ryan.a.yearsley@gmail.com>
2023-10-24 04:47:05 -0400

    Add TTTCornholeTileTrigger

---
+++ ToeTacTic_Unity/Assets/Scripts/CornHole/Components/TTTCornholeTileTrigger.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TTTCornholeTileTrigger : MonoBehaviour
+{
+	[SerializeField]
+	private Vector2Int tileCoordinate;
+	[SerializeField]
+	private Material neutralMaterial;
+	[SerializeField]
+	private Material xMaterial;
+	[SerializeField]
+	private Material oMaterial;
+
+	private Renderer _renderer;
+	private Player currentOccupyingPlayer = Player.EMPTY;
+	private List<TTTCornholeBagComponent> xOccupants = new List<TTTCornholeBagComponent>();
+	private List<TTTCornholeBagComponent> oOccupants = new List<TTTCornholeBagComponent>();
+
+	private void Awake()
+	{
+		_renderer = transform.parent.GetComponent<Renderer>();
+		_renderer.material = neutralMaterial;
+		currentOccupyingPlayer = Player.EMPTY;
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("TileTrigger: TriggerEnter Detected!");
+		TTTCornholeBagComponent newOccupant = other.GetComponent<TTTCornholeBagComponent>();
+		if (newOccupant != null)
+		{
+			if (newOccupant.GetPlayer() == Player.X)
+				xOccupants.Add(newOccupant);
+			else
+				oOccupants.Add(newOccupant);
+		}
+		CalculateWinningOccupant();
+	}
+	public void OnTriggerExit(Collider other)
+	{
+		Debug.Log("TileTrigger: TriggerExit Detected!");
+		TTTCornholeBagComponent leavingOccupant = other.GetComponent<TTTCornholeBagComponent>();
+		if (leavingOccupant != null)
+		{
+			if (leavingOccupant.GetPlayer() == Player.X)
+				xOccupants.Remove(leavingOccupant);
+			else
+				oOccupants.Remove(leavingOccupant);
+		}
+		CalculateWinningOccupant();
+	}
+
+	public void CalculateWinningOccupant()
+	{
+		if (xOccupants.Count == oOccupants.Count)
+		{
+			_renderer.material = neutralMaterial;
+			currentOccupyingPlayer = Player.EMPTY;
+		}
+		else if (xOccupants.Count > oOccupants.Count)
+		{
+			_renderer.material = xMaterial;
+			currentOccupyingPlayer = Player.X;
+		}
+		else
+		{
+			_renderer.material = oMaterial;
+			currentOccupyingPlayer = Player.O;
+		}
+		if (TicTacToeGameManager.instance != null)
+		{
+			TicTacToeGameManager.instance.UpdateBoard(currentOccupyingPlayer, tileCoordinate);
+		}
+	}
+}
